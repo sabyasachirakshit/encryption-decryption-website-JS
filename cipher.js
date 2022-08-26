7590c0feece3e1a542b7e11385b348f2ad7d7c0f
Sabyasachi Rakshit <sabyasachirakshitacs19@gmail.com>
2022-08-26 09:54:48 +0530

    reverse string added

--- cipher.js
+++ cipher.js
@@ -1,1 +1,14 @@
 console.log("Cipher JS File linked!");
+function stringRevEncDec() {
+  let myStr = document.getElementById("stringRev").value;
+  let revStr = [];
+  for (let index = 0; index < myStr.length; index++) {
+    const element = myStr[index];
+    revStr.push(element);
+  }
+  let reversedString = "";
+  for (let index = revStr.length - 1; index >= 0; index--) {
+    reversedString += revStr[index];
+  }
+  document.getElementById("outputStr").innerHTML = reversedString;
+}
